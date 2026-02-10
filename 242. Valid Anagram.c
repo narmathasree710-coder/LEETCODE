8889e137102aac12e12e1543c8ebc73dc3a222a8
narmathasree710-coder <narmathasree710@gmail.com>
2026-02-10 15:01:57 +0530

    Create 242. Valid Anagram.c

---
+++ 242. Valid Anagram.c
@@ -0,0 +1,19 @@
+Example 1:
+Input: s = "anagram", t = "nagaram"
+Output: true
+Example 2:
+Input: s = "rat", t = "car"
+Output: false
+
+bool isAnagram(char* s, char* t) {
+    int countS,countT;
+    for(char ch='a';ch<='z';ch++){
+        countS=0; countT=0;
+        for(int i=0;s[i];i++){
+            if(s[i]==ch){
+                countS++;}}
+        for(int j=0;t[j];j++){
+            if(t[j]==ch){
+                countT++;}}
+        if(countS!=countT) return false;}
+    return true;}
