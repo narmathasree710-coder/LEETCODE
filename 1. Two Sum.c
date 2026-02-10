445922b715e86b6c9f183467804f26828337199e
narmathasree710-coder <narmathasree710@gmail.com>
2026-02-10 11:52:45 +0530

    Create 1. Two Sum.c

---
+++ 1. Two Sum.c
@@ -0,0 +1,22 @@
+Example 1:
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+Input: nums = [3,3], target = 6
+Output: [0,1]
+
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    int* result = (int*)malloc(2 * sizeof(int));
+    for (int i = 0; i < numsSize; i++) {
+        for (int j = i + 1; j < numsSize; j++) {
+            if (nums[i] + nums[j] == target) {
+                result[0] = i;
+                result[1] = j;
+                *returnSize = 2;
+                return result;}}}
+    *returnSize = 0;
+    return result;}
