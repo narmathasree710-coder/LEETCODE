98c9090f760f6e411c00d3fe6df61750940c887f
narmathasree710-coder <narmathasree710@gmail.com>
2026-02-19 21:57:33 +0530

    Update 1456. Maximum Number of Vowels in a Substring of Given Length.c

--- 1456. Maximum Number of Vowels in a Substring of Given Length.c
+++ 1456. Maximum Number of Vowels in a Substring of Given Length.c
@@ -1,16 +1,3 @@
-Example 1:
-Input: s = "abciiidef", k = 3
-Output: 3
-Explanation: The substring "iii" contains 3 vowel letters.
-Example 2:
-Input: s = "aeiou", k = 2
-Output: 2
-Explanation: Any substring of length 2 contains 2 vowels.
-Example 3:
-Input: s = "leetcode", k = 3
-Output: 2
-Explanation: "lee", "eet" and "ode" contain 2 vowels.
-
 #include<stdio.h>
 int maxVowels(char* s, int k) {
     int n=strlen(s);
