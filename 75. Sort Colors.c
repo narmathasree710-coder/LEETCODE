683d0c60a56b3cdc3f2c26191a6ee5bd7e78361b
narmathasree710-coder <narmathasree710@gmail.com>
2026-02-19 20:37:54 +0530

    Update 75. Sort Colors.c

--- 75. Sort Colors.c
+++ 75. Sort Colors.c
@@ -1,10 +1,3 @@
-Example 1:
-Input: nums = [2,0,2,1,1,0]
-Output: [0,0,1,1,2,2]
-Example 2:
-Input: nums = [2,0,1]
-Output: [0,1,2]
-  
 void sortColors(int* nums, int numsSize) {
     int count0=0,count1=0,count2=0,value=0;
     for(int i=0;i<numsSize;i++){
