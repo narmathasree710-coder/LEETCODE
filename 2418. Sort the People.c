b43929a63fac6ee3f7702dac6d0f5dd997c5eab2
narmathasree710-coder <narmathasree710@gmail.com>
2026-02-19 21:57:56 +0530

    Update 2418. Sort the People.c

--- 2418. Sort the People.c
+++ 2418. Sort the People.c
@@ -1,12 +1,3 @@
-Example 1:
-Input: names = ["Mary","John","Emma"], heights = [180,165,170]
-Output: ["Mary","Emma","John"]
-Explanation: Mary is the tallest, followed by Emma and John.
-Example 2:
-Input: names = ["Alice","Bob","Bob"], heights = [155,185,150]
-Output: ["Bob","Alice","Bob"]
-Explanation: The first Bob is the tallest, followed by Alice and the second Bob.
-
 char** sortPeople(char** names, int namesSize, int* heights, int heightsSize, int* returnSize) {
     for(int i=0;i<namesSize;i++){
         for(int j=i+1;j<namesSize;j++){
