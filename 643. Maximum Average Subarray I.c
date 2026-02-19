abefed2a12f2e737637ab929c8856cea47d57661
narmathasree710-coder <narmathasree710@gmail.com>
2026-02-19 21:51:47 +0530

    Update 643. Maximum Average Subarray I.c

--- 643. Maximum Average Subarray I.c
+++ 643. Maximum Average Subarray I.c
@@ -1,11 +1,3 @@
-Example 1:
-Input: nums = [1,12,-5,-6,50,3], k = 4
-Output: 12.75000
-Explanation: Maximum average is (12 - 5 - 6 + 50) / 4 = 51 / 4 = 12.75
-Example 2:
-Input: nums = [5], k = 1
-Output: 5.00000
-
 double findMaxAverage(int* nums, int numsSize, int k) {
     int maxSum=0;
     int windowSum=0;
