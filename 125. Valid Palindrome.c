c5cf3a94bd9e47b7c4016b0f9ef4e441773d535c
narmathasree710-coder <narmathasree710@gmail.com>
2026-02-24 20:39:27 +0530

    Create 125. Valid Palindrome.c

---
+++ 125. Valid Palindrome.c
@@ -0,0 +1,9 @@
+#include <ctype.h>
+bool isPalindrome(char* s) {
+    int i=0,j=strlen(s)-1;
+    while(i<j){
+        while(i<j && !isalnum(s[i])) i++;
+        while(i<j && !isalnum(s[j])) j--;
+        if(tolower(s[i++]) != tolower(s[j--])){
+        return false;}
+    }return true;}
